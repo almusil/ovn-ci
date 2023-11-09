feaca080f8ad703598a11a591475afec1a4f4b27
Ales Musil <amusil@redhat.com>
2023-11-09 09:11:35 +0100

    ovn-ci: Add extension to avoid some code repetition
    The common pattern of checking command output status
    can be extracted into trait extension which can be
    reused by any command ending with output() call.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- lib/container.rs
+++ lib/container.rs
@@ -3,12 +3,14 @@ use std::process::Command;
 
 use thiserror::Error as ThisError;
 
+use crate::util::OutputExt;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(ThisError, Debug)]
 pub enum Error {
     #[error("Container command failed: {0}")]
-    Command(#[source] IoError),
+    Command(#[from] IoError),
     #[error("Cannot pull \"{0}\": {1}")]
     ImagePull(String, String),
 }
@@ -23,17 +25,11 @@ impl<'a> Container<'a> {
     }
 
     pub fn pull(&self) -> Result<()> {
-        let output = Command::new("podman")
+        Command::new("podman")
             .arg("pull")
             .arg(self.image)
-            .output()
-            .map_err(Error::Command)?;
-
-        if !output.status.success() {
-            let stderr = String::from_utf8_lossy(&output.stderr).to_string();
-            return Err(Error::ImagePull(self.image.to_string(), stderr));
-        }
-
-        Ok(())
+            .output()?
+            .status_ok()
+            .map_err(|e| Error::ImagePull(self.image.to_string(), e))
     }
 }
