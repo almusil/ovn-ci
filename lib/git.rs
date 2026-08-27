1c4938083b7a10d1031bb351e2eec1a7ecaae969
Ales Musil <amusil@redhat.com>
2026-08-27 09:22:24 +0200

    git: Make sure we update all available branches.
    In case we have a repository with multiple branches update
    all of them. This is needed for upgrade suite that is
    relaying on the fact that we have up-to-date branches.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- lib/git.rs
+++ lib/git.rs
@@ -19,6 +19,12 @@ pub enum Error {
     Update(String, String),
     #[error("Cannot determine commit hash \"{0}\": {1}")]
     CommitHash(String, String),
+    #[error("Cannot list available branches \"{0}\": {1}")]
+    ListBranches(String, String),
+    #[error("Cannot checkout branch \"{0}\": {1}")]
+    Checkout(String, String),
+    #[error("HEAD is detached, cannot update branches \"{0}\": {1}")]
+    Detached(String, String),
 }
 
 pub struct Git<'a> {
@@ -33,9 +39,20 @@ impl<'a> Git<'a> {
     pub fn update(&self) -> Result<()> {
         let parent = self.submodule_parent()?;
         if parent.is_empty() {
-            self.update_project_command()?;
+            let current = self.current_branch()?;
+
+            if current.is_empty() {
+                return Err(Error::Detached(self.path.to_string(), self.commit_hash()?));
+            }
+
+            for branch in self.list_branches()?.lines() {
+                self.checkout(branch)?;
+                self.update_project()?;
+                self.update_submodule(self.path)?;
+            }
+            self.checkout(&current)?;
         } else {
-            self.update_submodule_command(&parent)?;
+            self.update_submodule(&parent)?;
         }
 
         Ok(())
@@ -69,7 +86,47 @@ impl<'a> Git<'a> {
         Ok(stdout)
     }
 
-    fn update_submodule_command(&self, workdir: &str) -> Result<()> {
+    fn list_branches(&self) -> Result<String> {
+        let stdout = Command::new("git")
+            .arg("branch")
+            .arg("--no-color")
+            .arg("--format=%(refname:short)")
+            .current_dir(self.path)
+            .output()?
+            .stdout()
+            .map_err(|e| Error::ListBranches(self.path.to_string(), e))?
+            .trim_end()
+            .to_string();
+
+        Ok(stdout)
+    }
+
+    fn current_branch(&self) -> Result<String> {
+        let stdout = Command::new("git")
+            .arg("branch")
+            .arg("--no-color")
+            .arg("--show-current")
+            .current_dir(self.path)
+            .output()?
+            .stdout()
+            .map_err(|e| Error::ListBranches(self.path.to_string(), e))?
+            .trim_end()
+            .to_string();
+
+        Ok(stdout)
+    }
+
+    fn checkout(&self, branch: &str) -> Result<()> {
+        Command::new("git")
+            .arg("checkout")
+            .arg(branch)
+            .current_dir(self.path)
+            .output()?
+            .status_ok()
+            .map_err(|e| Error::Checkout(self.path.to_string(), e))
+    }
+
+    fn update_submodule(&self, workdir: &str) -> Result<()> {
         Command::new("git")
             .arg("submodule")
             .arg("update")
@@ -80,7 +137,7 @@ impl<'a> Git<'a> {
             .map_err(|e| Error::SubmoduleUpdate(self.path.to_string(), e))
     }
 
-    fn update_project_command(&self) -> Result<()> {
+    fn update_project(&self) -> Result<()> {
         Command::new("git")
             .arg("pull")
             .arg("--rebase")
